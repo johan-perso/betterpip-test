dbf507a13590380a974dbf166ac2257a281a07c8
Johan le stickman <Johan_Stickman@proton.me>
2022-04-30 09:57:07 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import chalk
 
-print(f"Je suis exécuté depuis : {os.getcwd()}")
-print(f"et je suis : {__file__}")
+print(f"Je suis exécuté depuis : {chalk.blue(os.getcwd())}")
+print(f"et je suis : {chalk.blue(__file__)}")
